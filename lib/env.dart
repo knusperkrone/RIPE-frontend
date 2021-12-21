e9fb5ae2fa91785c1050edd6005242ec10c4491e
knusperkrone <aron.metzig@fau.de>
2021-12-22 00:03:59 +0100

    Changed BACKEND_URL

--- lib/env.dart
+++ lib/env.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/foundation.dart';
 
-const BACKEND_URL = kReleaseMode ? 'https://integration.if-lab.de/arme-iftem-backend' : 'http://10.0.2.2:8000' ;
+const BACKEND_URL = 'https://retroapp.if-lab.de:8000';
 const SENTRY_DSN = 'https://2518fe9ebaaa43d8b2aa9c52e0a59974@sentry.if-lab.de/14';
